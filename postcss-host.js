3c05fbc98d75e451bab9c62d7e8692c8af6fad16
jtzero <jthomaszero511@yahoo.com>
2016-07-14 16:05:59 -0600

    indentation lint fixes

--- postcss-host.js
+++ postcss-host.js
@@ -2,10 +2,10 @@ var postcss = require('postcss');
 
 module.exports = postcss.plugin('postcss-host', function () {
     return function (css) {
-       css.walkRules(function (rule) {
-           var replaced = [];
-           rule.selectors.forEach(function (selector) {
-                var idx = selector.indexOf(':host');
+        css.walkRules(function (rule) {
+            var replaced = [];
+            rule.selectors.forEach(function (selector) {
+            var idx = selector.indexOf(':host');
                 var hostCtxIdx = selector.indexOf(':host-context');
                 if (idx === -1 && !(hostCtxIdx === -1 && hostCtxIdx !== idx)) {
                   replaced.push(':host ' + selector);
