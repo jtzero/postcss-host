3f40f8f0fcb23e29bf1e9bdb89d3279a98753b8a
jtzero <jthomaszero511@yahoo.com>
2016-07-14 16:09:15 -0600

    missed more indendation

--- postcss-host.js
+++ postcss-host.js
@@ -5,13 +5,13 @@ module.exports = postcss.plugin('postcss-host', function () {
         css.walkRules(function (rule) {
             var replaced = [];
             rule.selectors.forEach(function (selector) {
-            var idx = selector.indexOf(':host');
-                var hostCtxIdx = selector.indexOf(':host-context');
-                if (idx === -1 && !(hostCtxIdx === -1 && hostCtxIdx !== idx)) {
-                  replaced.push(':host ' + selector);
-                } else {
-                  replaced.push(selector);
-                }
+                var idx = selector.indexOf(':host');
+                    var hostCtxIdx = selector.indexOf(':host-context');
+                    if (idx === -1 && !(hostCtxIdx === -1 && hostCtxIdx !== idx)) {
+                        replaced.push(':host ' + selector);
+                    } else {
+                        replaced.push(selector);
+                    }
            });
            if ( replaced.length ) {
                rule.selectors = replaced;
