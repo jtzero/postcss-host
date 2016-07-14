d6f65b82e80328277fdddc4037de59687653afbe
jtzero <jthomaszero511@yahoo.com>
2016-07-14 16:14:32 -0600

    again, indentation

--- postcss-host.js
+++ postcss-host.js
@@ -6,12 +6,12 @@ module.exports = postcss.plugin('postcss-host', function () {
             var replaced = [];
             rule.selectors.forEach(function (selector) {
                 var idx = selector.indexOf(':host');
-                    var hostCtxIdx = selector.indexOf(':host-context');
-                    if (idx === -1 && !(hostCtxIdx === -1 && hostCtxIdx !== idx)) {
-                        replaced.push(':host ' + selector);
-                    } else {
-                        replaced.push(selector);
-                    }
+                var hostCtxIdx = selector.indexOf(':host-context');
+                if (idx === -1 && !(hostCtxIdx === -1 && hostCtxIdx !== idx)) {
+                    replaced.push(':host ' + selector);
+                } else {
+                    replaced.push(selector);
+                }
            });
            if ( replaced.length ) {
                rule.selectors = replaced;
